63eac5869f837395f80b62a88c460b5115382720
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-08-16 00:21:23 +0530

    fixed the video uploads ui and upload video's preview section

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,23 +1,17 @@
 import VideosDisplay from "../components/Videos/videosDisplay";
 import VideosUpload from "../components/Videos/videosUpload";
 
-
-
-const Videos:React.FC = () => {
-
-
-    return (
-        <div className="flex px-20 
-        py-10  bg-black/30 mx-1 text-white w-full h-full  min-h-screen flex-col gap-5 font-grotesk">
-             <div>
-        <h1 className="text-3xl font-semibold my-2 ">VIDEO CLIPS</h1>
-       <hr className="w-full h-[2px] border-0 bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 " />
-
-               </div>
-            <VideosDisplay></VideosDisplay>
-            <VideosUpload></VideosUpload>
-        </div>
-    )
-}
+const Videos: React.FC = () => {
+  return (
+    <div className="flex px-8 py-4 bg-[#0d0d0fd6] mx-1 text-white w-full h-full min-h-screen flex-col gap-5 font-geist">
+      <div>
+        <h1 className="text-2xl font-semibold my-2">VIDEO CLIPS</h1>
+        <hr className="w-full h-[2px] border-0 bg-gradient-to-r from-blue-500 via-purple-500 to-blue-500" />
+      </div>
+      <VideosDisplay />
+      <VideosUpload />
+    </div>
+  );
+};
 
 export default Videos;
